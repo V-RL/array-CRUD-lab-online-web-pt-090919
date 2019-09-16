c3d069b86a87cc8748a51401f97d59b9b0720a7c
Brandon Furr <brandon.furr123@gmail.com>
2019-09-16 04:57:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   arrays = ["this", "is", "an", "array"]
-  arrays << "hello"
+  arrays << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
